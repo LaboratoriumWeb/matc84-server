b9b97314fd4dbb56f676b37ed9b6c9bb35345931
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-14 14:48:11 -0300

    fix: adjust logic for better performance

--- api/app.js
+++ api/app.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const initializeDatabase = require("./database/database.js");
+const {connection, authenticate} = require("./database/database.js");
 const app = express();
-const port = process.env.APP_PORT;
+const cors = require("cors");
+const authRoutes = require("./routes/auth.js");
+const userRoutes = require("./routes/user.js");
+
+app.use(cors());
 
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
-(async () => {
-    const connection = await initializeDatabase();    
-    try {
-        await connection.sync();
-    } catch (error) {
-        console.error("Error during database synchronization:", error);
-    }
 
-    app.listen(port, () => {
-        console.log(`Server running on port http://localhost:${port}/`);
-    });
-})();
+authenticate(connection);
+connection.sync();
 
-module.exports = app;
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${process.env.APP_PORT}/`);
+});
