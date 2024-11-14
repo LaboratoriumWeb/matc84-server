c8decd41000562593b8627ade64cf7b8bddc8057
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-14 14:48:28 -0300

    fix: adjust connection to db

--- api/database/database.js
+++ api/database/database.js
@@ -1,40 +1,19 @@
-const {Sequelize} = require("sequelize");
-const mysql = require("mysql2/promise");
+const { Sequelize } = require("sequelize");
 
-
-async function initializeDatabase() {
-    const connection = await mysql.createConnection({
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-    });
-
-    // Verifica e cria o banco de dados, se necessário
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
-    await connection.end();
-
-    // Conexão final com o banco de dados especificado
-    const sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            timezone: '-03:00',
-            logging: false,
-        }
-    );
-
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
+        dialect: "mysql",
+        timezone: "-03:00",
     }
+);
 
-    return sequelize;
+async function authenticate(connection) {
+    try {
+        await connection.authenticate();
+        console.log("Conexão estabelecida com sucesso!");
+    } catch (err) {
+        console.log("Um erro inesperado aconteceu: ", err);
+    }
 }
 
-
-module.exports = initializeDatabase;
+module.exports = { connection, authenticate };
